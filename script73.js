85bc2222ab1ed59428492f279d88a64d8d58dbb1
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-08-27 20:58:45 -0400

    Add files via upload

---
+++ script73.js
@@ -0,0 +1,30 @@
+// var count = -10;
+
+// while (count < 19) {
+//     document.write(count);
+//     count++;
+// }
+
+// var count = 10;
+
+// while (count < 40) {
+//     if (count % 2 == 0)
+//         document.write(count);
+//     count++;
+// }
+
+// var count = 300;
+
+// while (count < 333) {
+//     if (count % 2 !== 0)
+//         document.write(count);
+//     count++;
+// }
+
+var count = 5;
+
+while (count < 50) {
+    if ((count % 5 == 0) && (count % 3 == 0))
+        console.log(count);
+    count++;
+}
